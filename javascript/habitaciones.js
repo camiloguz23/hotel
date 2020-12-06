ac73180e206733412f6543be18b89d45c8c39e2d
Holwan david montes <holmanmontesmontes57@gmail.com>
2020-12-06 11:38:38 -0500

    arreglando los listados de ofertas

--- javascript/habitaciones.js
+++ javascript/habitaciones.js
@@ -1,14 +1,95 @@
-const btnEsconder1 = document.getElementById('serviciosID2')
-const btnEsconder2 = document.getElementById('serviciosID1')
-const esconder2 = document.getElementById('listaDeServicios2')
-const esconder1 = document.getElementById('listaDeServicios1')
+const btnEsconder2 = document.getElementById('serviciosID2');
+const esconder2 = document.getElementById('listaDeServicios2');
 
+const btnEsconder1 = document.getElementById('serviciosID1');
+const esconder1 = document.getElementById('listaDeServicios1');
+
+const btnEsconder3 = document.getElementById('serviciosID3');
+const esconder3 = document.getElementById('listaDeservicios3');
+
+const btnEsconder4 = document.getElementById('serviciosID4');
+const esconder4 = document.getElementById('listaDeServicios4');
+
+const btnEsconder5 = document.getElementById('serviciosID5');
+const esconder5 = document.getElementById('listaDeServicios5');
+
+const btnEsconder6 = document.getElementById('serviciosID6');
+const esconder6 = document.getElementById('listaDeServicios6');
+
+const btnEsconder7 = document.getElementById('serviciosID7');
+const esconder7 = document.getElementById('listaDeServicios7');
+
+const btnEsconder8 = document.getElementById('serviciosID8');
+const esconder8 = document.getElementById('listaDeServicios8');
+
+const btnEsconder9 = document.getElementById('serviciosID9');
+const esconder9 = document.getElementById('listaDeServicios9');
+
+const btnEsconder10 = document.getElementById('serviciosID10');
+const esconder10 = document.getElementById('listaDeServicios10');
+
+btnEsconder10.addEventListener('click',()=>{
+    esconder10.classList.toggle('listaDeServicios');
+})
+
+btnEsconder9.addEventListener('click', ()=>{
+    esconder9.classList.toggle('listaDeServicios');
+})
+
+btnEsconder8.addEventListener('click',()=>{
+    esconder8.classList.toggle('listaDeServicios');
+})
+
+btnEsconder7.addEventListener('click',()=>{
+    esconder7.classList.toggle('listaDeServicios');
+    
+})
+
+
+btnEsconder6.addEventListener('click', ()=>{
+    esconder6.classList.toggle('listaDeServicios');
+})
+
+
+btnEsconder5.addEventListener('click',()=>{
+    esconder5.classList.toggle('listaDeServicios');
+})
+
+btnEsconder4.addEventListener('click',()=>{
+    esconder4.classList.toggle('listaDeServicios');
+    
+})
+
+/*------------------------comenzar a esconder-------------- */
 btnEsconder1.addEventListener('click',()=>{
-
-    esconder2.classList.toggle('listaDeServicios');
+    esconder1.classList.toggle('listaDeServicios');
 } )
 
 btnEsconder2.addEventListener('click',()=>{
-
-    esconder1.classList.toggle('listaDeServicios');
+    esconder2.classList.toggle('listaDeServicios');
 } )
+
+btnEsconder3.addEventListener('click',()=>{
+    esconder3.classList.toggle('listaDeServicios')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
